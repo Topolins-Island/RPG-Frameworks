97896da127ccacf96139bbd34061d5f7c7af91df
beheeyem <beheeyemevolved03@gmail.com>
2019-08-25 12:40:00 -0700

    Update BattleActor.cs

--- RealTime/BattleActor.cs
+++ RealTime/BattleActor.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //This class is the parent of everything that could possibly act,
-//since all actors will be adding themselves to the line
+//since all actors will be adding themselves to the line.
+//For instance, a damage over time effect would inherit from this,
+//and add itself to the line whenever it needs to inflict damage.
 public class BattleActor : MonoBehaviour
 {
     public virtual void Act()
