c7272d3e50dcd580f198913a8e55abf65fe510bc
beheeyem <beheeyemevolved03@gmail.com>
2019-08-25 11:58:08 -0700

    Update PlayerUnitController.cs

--- RegularTurnBased/PlayerUnitController.cs
+++ RegularTurnBased/PlayerUnitController.cs
@@ -7,6 +7,11 @@ public class PlayerUnitController : MonoBehaviour
 {
     public List<Button> abilityButtons;
 
+    PlayerUnit current;
+
+    //This is called by player units when they need to get input from the player
+    //This fucntion sets up a series of buttons in the scene to correlate to each
+    //of the unit's abilities.
     public void DisplayAbilities(PlayerUnit me, List<Ability> abilities)
     {
         current = me;
@@ -27,12 +32,15 @@ public class PlayerUnitController : MonoBehaviour
         }
     }
 
+    //This is an intermediary function that is called when the buttons are pressed.
+    //It is assigned to these buttons dynamically by the function above.
     public void AbilityChosen(Ability ability)
     {
         current.AbilityChosen(ability);
         MakeAbilityButtonsDisappear();
     }
 
+    //This function makes the buttons disappears and clears the onClick event
     void MakeAbilityButtonsDisappear()
     {
         for (int i = 0; i < abilityButtons.Count; i++)
@@ -41,8 +49,8 @@ public class PlayerUnitController : MonoBehaviour
             abilityButtons[i].onClick.RemoveAllListeners();
         }
     }
-
-    PlayerUnit current;
+    
+    //This function displays an button (the targets first child) and gives it a function
     public void PaintTargets(PlayerUnit me, Unit target)
     {
         current = me;
@@ -52,36 +60,12 @@ public class PlayerUnitController : MonoBehaviour
         select.GetComponent<Button>().onClick.AddListener(delegate { Clicked(select, target); });
     }
 
+    //This function takes the clicked gameobject and the unit it represents.
+    //It makes the gameobject disappear and tells the player the gameobject's unit was selected.
     public void Clicked(GameObject me, Unit selected)
     {
         //print(me.name + " was clicked.");
         me.SetActive(false);
         current.PlayerSelected(selected);
     }
-}
-
-public class PlayerUnitControllerEp03 : MonoBehaviour
-{
-    PlayerUnit current;
-    public void PaintTargets(PlayerUnit me, Unit target)
-    {
-        current = me;
-        GameObject select = target.transform.GetChild(0).gameObject;
-        select.SetActive(true);
-        select.GetComponent<Button>().onClick.RemoveAllListeners();
-        select.GetComponent<Button>().onClick.AddListener(delegate { Clicked(select, target); });
-    }
-
-    public void Clicked(GameObject me, Unit selected)
-    {
-        //print(me.name + " was clicked.");
-        me.SetActive(false);
-        current.PlayerSelected(selected);
-    }
-}
-
-public class AbilityButtons
-{
-    public GameObject abilityBackground;
-    public Text abilityName;
 }
