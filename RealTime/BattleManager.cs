4ff8b0566c783168d5b4c551226bfb7c6de8bae0
beheeyem <beheeyemevolved03@gmail.com>
2019-08-25 13:01:12 -0700

    Update BattleManager.cs

--- RealTime/BattleManager.cs
+++ RealTime/BattleManager.cs
@@ -16,6 +16,7 @@ public class BattleManager : MonoBehaviour
 
     BattleActor current;
 
+    //A temporary starting function
     private void Awake()
     {
         instance = this;
@@ -24,49 +25,61 @@ public class BattleManager : MonoBehaviour
         StartCoroutine(LineProcessing());
     }
 
+    //An actor acts itself to the line
     public void UnitReady(BattleActor me)
     {
         line.Add(me);
     }
 
+    //A unit dies and it's stuff is removed here
+    //Another version needs to be made for actors
     public void UnitDied(Unit me)
     {
         units.Remove(me);
         if (line.Contains(me))
             line.Remove(me);
     }
-
-    bool currentGoing = false;
+    
+    //A coroutine that handles the real time portion of the battle mechanics
     IEnumerator LineProcessing()
     {
-        //this should be replaced by checking for unit death
+        //Before each loop we check if the battle is over
         while (!IsBattleOver())
         {
+            //If the line is empty, we wait until someone joins
             while (line.Count == 0)
             {
                 yield return null;
             }
 
+            //Set the controlling boolean to true
             currentGoing = true;
-
+            
+            //Pop the first one in line
             current = line[0];
             line.RemoveAt(0);
+            //Tell the popped unit to go
             current.Act();
 
+            //Wait for the current to be done
             while (currentGoing)
                 yield return null;
         }
         
+        //Once we escape the above loop, the battle is over
         BattleEnded();
     }
 
+    //This function returns true if the battle is over
     bool IsBattleOver()
     {
-        bool bep = false;
+        bool over = false;
 
         bool enemiesAlive = false;
         bool playersAlive = false;
 
+        //We go through the units list in order to determine who is still alive.
+        //We determine which side their own based on what their parent object is.
         foreach(Unit u in units)
         {
             if (u.transform.parent == playerHolder)
@@ -76,11 +89,13 @@ public class BattleManager : MonoBehaviour
         }
 
         if (playersAlive != enemiesAlive)
-            bep = true;
+            over = true;
 
-        return bep;
+        return over;
     }
-
+    
+    //A controlling bool, allowing the units to tell the manager when they are done by calling this function
+    bool currentGoing = false;
     public void Continue()
     {
         currentGoing = false;
@@ -108,6 +123,7 @@ public class BattleManager : MonoBehaviour
         return playas;
     }
 
+    //This is a simple method to check who won the battle
     void BattleEnded()
     {
         foreach (Unit u in units)
